34bf3c539f1f4318551816b59795cdc66a6a448a
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-05 20:35:10 -0700

    added sync sequelize models to the database

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: true }).then(() => {
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
