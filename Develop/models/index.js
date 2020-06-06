64dce81f5eefcc3f558eb8bf50da469449b461b2
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-05 21:17:41 -0700

    saving last changes before testing apis

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -18,14 +18,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  // as: 'product_tag',
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  // as: 'product_tag',
   foreignKey: 'tag_id'
 })
 
