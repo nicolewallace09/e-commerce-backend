590446d03d6301aaa44c6cb70750afa51ad07a17
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-11 21:45:49 -0700

    adding new video

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -26,6 +26,9 @@ router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   Tag.findOne({
+    where: {
+      id: req.params.id
+    },
     include: [
       {
         model: Product,
